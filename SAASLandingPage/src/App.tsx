7129e95773ad6d62b0fe3bf88258154bf455b64c
AdelAulia#7113 <adelauliaw@gmail.com>
2024-10-13 18:56:34 +0700

    setup features and hero

--- SAASLandingPage/src/App.tsx
+++ SAASLandingPage/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from './sections/header.jsx'
-
+import React from "react";
+import Header from "./sections/Header.tsx";
+import Hero from "./sections/Hero.tsx";
+import Features from "./sections/Features.tsx";
 
 const App = () => {
   return (
-    <main className='overflow-hidden'>
-
-      <Header/>
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
